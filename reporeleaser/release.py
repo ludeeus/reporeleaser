79c27bb0674b4c9f3ec31790a753cdd950a0c230
ludeeus <joasoe@gmail.com>
2019-01-08 01:18:34 +0100

    Better handling of version defaults

--- reporeleaser/release.py
+++ reporeleaser/release.py
@@ -85,7 +85,6 @@ class CreateRelease():
         major = 0
         minor = 0
         patch = 0
-        beta = 0
 
         if self.release not in RELEASETYPES:
             version = self.release
@@ -99,18 +98,15 @@ class CreateRelease():
 
             if self.release == 'major':
                 major = int(current_version[0]) + 1
+
             elif self.release == 'minor':
+                major = current_version[0]
                 minor = int(current_version[1]) + 1
+
             elif self.release == 'patch':
+                major = current_version[0]
+                minor = current_version[1]
                 patch = int(current_version[2]) + 1
-            elif self.release == 'beta':
-                splitindex = segments - 1
-                if 'b' in current_version[splitindex]:
-                    betasplit = current_version[splitindex].split('b')
-                    beta = int(betasplit[0]) + 1
-                    patch = str(betasplit[0]) + 'b' + str(beta)
-                else:
-                    patch = "{}b{}".format(0, 0)
 
             if segments == 2:
                 version = "{}.{}".format(major, minor)
@@ -127,11 +123,11 @@ class CreateRelease():
         tag_sha = None
         data = {}
         tags = list(self.repo_obj.get_tags())
+        reg = "(v|^)?(\\d+\\.)?(\\d+\\.)?(\\*|\\d+)$"
         if tags:
             data['tags'] = True
             for tag in tags:
                 tag_name = tag.name
-                reg = "(v|^)?(\\d+\\.)?(\\d+\\.)?(\\*|\\d+|\\d+b+\\d)$"
                 if re.match(reg, tag_name):
                     tag_sha = tag.commit.sha
                     break
@@ -139,7 +135,7 @@ class CreateRelease():
                 if self.release in RELEASETYPES:
                     tag_name = None
                     message = "Could not find a previous tag matching "
-                    message += "(v)X.X(.X)(bX)"
+                    message += "(v)X.X(.X)"
                     print(message)
         else:
             data['tags'] = False
